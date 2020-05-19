cfec3e294500f107592987609bc04561e90574a4
Orion Dollar <odollar@uw.edu>
2020-05-19 10:58:19 -0700

    vae_updates

--- modules/vae_generator.py
+++ modules/vae_generator.py
@@ -12,17 +12,17 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 # keras
-# import keras
-# from keras.models import Sequential, Model
-# from keras.optimizers import Adam
-# from keras import backend as K
-# from keras import objectives
-# from keras.objectives import binary_crossentropy #objs or losses
-# from keras.layers import Dense, Dropout, Input, Multiply, Add, Lambda, concatenate
-# from keras.layers.core import Dense, Activation, Flatten, RepeatVector
-# from keras.layers.wrappers import TimeDistributed
-# from keras.layers.recurrent import GRU
-# from keras.layers.convolutional import Convolution1D
+import keras
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+from keras import backend as K
+from keras import objectives
+from keras.objectives import binary_crossentropy #objs or losses
+from keras.layers import Dense, Dropout, Input, Multiply, Add, Lambda, concatenate
+from keras.layers.core import Dense, Activation, Flatten, RepeatVector
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.recurrent import GRU
+from keras.layers.convolutional import Convolution1D
 
 # me
 import util.util as uu
@@ -284,7 +284,6 @@ class PlastVAEGen():
                         log_file = open('trials/log{}.txt'.format('_'+self.name), 'a')
                     else:
                         log_file = open('trials/log.txt', 'a')
-                    log_file = open('log.txt', 'a')
                     log_file.write('{},{},{},{},{},{}\n'.format(self.n_epochs,batch_idx,'train',loss.item(),bce.item(),kld.item()))
                     log_file.close()
                 # print('{},{},{},{},{},{}\n'.format(epoch,batch_idx,'train',loss.item(),bce.item(),kld.item()))
@@ -313,7 +312,6 @@ class PlastVAEGen():
                         log_file = open('trials/log{}.txt'.format('_'+self.name), 'a')
                     else:
                         log_file = open('trials/log.txt', 'a')
-                    log_file = open('log.txt', 'a')
                     log_file.write('{},{},{},{},{},{}\n'.format(self.n_epochs,batch_idx,'test',loss.item(),bce.item(),kld.item()))
                     log_file.close()
                 # print('{},{},{},{},{},{}\n'.format(epoch,batch_idx,'test',loss.item(),bce.item(),kld.item()))
@@ -677,9 +675,115 @@ class PlastVAEGen_tf():
         self.verbose = verbose
         self.params = params
         self.name = name
-        if 'LATENT_SIZE' in self.params.keys():
-            self.latent_size = self.params['LATENT_SIZE']
-        else:
-            self.latent_size = 512
+        if 'LATENT_SIZE' not in self.params.keys():
+            self.params['LATENT_SIZE'] = 512
         if 'KL_BETA' not in self.params.keys():
             self.params['KL_BETA'] = 1.0
+
+    def initiate(self, data):
+        """
+        This function not only loads data, but also builds the network
+        architecture. This must be done after the data is loaded because
+        the number of input channels is dependent on the max length of smiles
+        strings and number of unique characters.
+        """
+        # Setting up parameters
+        self.all_smiles = data[:,0]
+        self.all_lls = data[:,1]
+        self.params['DATA_LENGTH'] = max(map(len, data[:,0]))
+        if 'MAX_LENGTH' not in self.params.keys():
+            self.params['MAX_LENGTH'] = int(self.params['DATA_LENGTH'] * 1.5)
+        if 'TRAIN_SPLIT' not in self.params.keys():
+            self.params['TRAIN_SPLIT'] = 0.8
+
+        # One-hot encoding smiles below the max length
+        self.usable_data = [(ll, sm) for ll, sm in zip(self.all_lls, self.all_smiles) if len(sm) < self.params['MAX_LENGTH']]
+        self.usable_lls = np.array([x[0] for x in self.usable_data])
+        self.usable_smiles = [x[1] for x in self.usable_data]
+        self.params['CHAR_DICT'], self.params['ORD_DICT'] = uu.get_smiles_vocab(self.usable_smiles)
+        self.params['NUM_CHAR'] = len(self.params['CHAR_DICT'])
+        self.params['PAD_NUM'] = self.params['CHAR_DICT']['_']
+        self.encoded = torch.empty((len(self.usable_smiles), self.params['NUM_CHAR'], self.params['MAX_LENGTH']))
+        for i, sm in enumerate(self.usable_smiles):
+            self.encoded[i,:,:] = torch.tensor(uu.encode_smiles(sm, self.params['MAX_LENGTH'], self.params['CHAR_DICT']))
+        self.encoded = self.encoded.permute(0, 2, 1)
+        self.input_shape = (self.params['NUM_CHAR'], self.params['MAX_LENGTH'])
+
+        # Data preparation
+        self.params['N_SAMPLES'] = self.encoded.shape[0]
+        self.params['N_TRAIN'] = int(self.params['N_SAMPLES'] * self.params['TRAIN_SPLIT'])
+        self.params['N_TEST'] = self.params['N_SAMPLES'] - self.params['N_TRAIN']
+        self.rand_idxs = np.random.choice(np.arange(self.params['N_SAMPLES']), size=self.params['N_SAMPLES'])
+        self.params['TRAIN_IDXS'] = self.rand_idxs[:self.params['N_TRAIN']]
+        self.params['VAL_IDXS'] = self.rand_idxs[self.params['N_TRAIN']:]
+
+        self.X_train = self.encoded[self.params['TRAIN_IDXS'],:,:]
+        self.X_val = self.encoded[self.params['VAL_IDXS'],:,:]
+        self.y_train = self.usable_lls[self.params['TRAIN_IDXS']]
+        self.y_val = self.usable_lls[self.params['VAL_IDXS']]
+
+        x = Input(shape=(self.params['MAX_LENGTH'], self.params['NUM_CHAR']))
+
+        _, z = self._buildEncoder(x, self.params['LATENT_SIZE'], self.params['MAX_LENGTH'])
+
+        self.encoder = Model(x, z)
+        encoded_input = Input(shape=(self.params['LATENT_SIZE'],), name='encoded_input')
+        self.decoder = Model(encoded_input,
+                             self._buildDecoder(encoded_input,
+                                                self.params['LATENT_SIZE'],
+                                                self.params['MAX_LENGTH'],
+                                                self.params['NUM_CHAR']))
+
+        vae_loss, z1 = self._buildEncoder(x,
+                                          self.params['LATENT_SIZE'],
+                                          self.params['MAX_LENGTH'])
+
+        self.autoencoder = Model(x,
+                               self._buildDecoder(z1,
+                                                  self.params['LATENT_SIZE'],
+                                                  self.params['MAX_LENGTH'],
+                                                  self.params['NUM_CHAR']))
+
+        self.autoencoder.compile(optimizer = 'Adam',
+                                 loss = {'decoded_mean': vae_loss},
+                                 metrics = ['accuracy'])
+
+    def _buildEncoder(self, x, latent_size, max_len, eps_std=0.01,
+                      conv_layers=3, conv_filters=9, conv_kernel_size=9):
+        h = Convolution1D(9, 9, activation='relu', name='conv_1')(x)
+        for convolution in range(conv_layers-2):
+            h = Convolution1D(conv_filters, conv_kernel_size, activation='relu',
+                              name='conv_{}'.format(convolution+2))(h)
+        h = Convolution1D(10, 11, activation='relu', name='conv_{}'.format(conv_layers))(h)
+        h = Flatten(name='flatten_1')(h)
+        h = Dense(435, activation='relu', name='dense_1')(x)
+
+        def sampling(args):
+            mu_, logvar_ = args
+            batch_size = K.shape(mu_)[0]
+            eps = K.random_normal(shape=(batch_size, latent_size), mean=0., stddev=eps_std)
+            return mu_ + K.exp(logvar_ / 2) * eps
+
+        mu = Dense(latent_size, name='z_mean', activation='linear')(h)
+        logvar = Dense(latent_size, name='logvar', activation='linear')(h)
+
+        def vae_loss(x, x_decode):
+            x = K.flatten(x)
+            x_decode = K.flatten(x_decode)
+            BCE = max_len * objectives.binary_crossentropy(x, x_decode)
+            KLD = -0.5 * K.mean(1 + logvar - K.square(mu) - K.exp(logvar), axis=-1)
+            return BCE + KLD
+
+        return (vae_loss, Lambda(sampling, output_shape=(latent_size,), name='lamda')([mu, logvar]))
+
+    def _buildDecoder(self, z, latent_size, max_len, num_char,
+                      gru_layers=3, hidden_size=501):
+        h = Dense(latent_size, name='latent_input', activation='relu')(z)
+        h = RepeatVector(max_len, name='repeat_vector')(h)
+        h = GRU(hidden_size, return_sequences=True, name='gru_1')(h)
+        for gru in range(gru_layers-2):
+            h = GRU(hidden_size, return_sequences=True, name='gru_{}'.format(gru+2))(h)
+        h = GRU(hidden_size, return_sequences=True, name='gru_{}'.format(gru_layers))(h)
+        smiles_decoded = TimeDistributed(Dense(num_char, activation='softmax'),
+                                         name='decoded_mean')(h)
+        return smiles_decoded
